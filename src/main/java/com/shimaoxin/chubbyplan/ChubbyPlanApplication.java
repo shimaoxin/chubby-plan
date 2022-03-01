7248e8eb68d6ac120430ada0d5b0397c24214546
shimaoxin <wRFN40K2>
2022-03-01 09:19:48 +0800

    Initial commit

---
+++ src/main/java/com/shimaoxin/chubbyplan/ChubbyPlanApplication.java
@@ -0,0 +1,13 @@
+package com.shimaoxin.chubbyplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChubbyPlanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChubbyPlanApplication.class, args);
+    }
+
+}
